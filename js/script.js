e76d8c330f04ecd0ba30ce44015e4b9516ba7037
Roro BERNARD <mr.rodolphe.bernard@gmail.com>
2022-06-30 11:19:39 +0200

    MAJ 0.4 ( add contact.html propos.html )

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+let i = 0;
+let txt = 'Nous faisons tout pour satisfaire nos clients.';
+let speed = 30;
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("description").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
 $(document).ready(function(){
     $('li').click(function(){
         $('li').css('color','black');
